28b331c49c8287607591efaef32f6f81eff04cc3
krmr <3511772+krmr@users.noreply.github.com>
2021-12-13 15:14:35 +0100

    Initial commit

---
+++ ClassLibrary/RecursiveClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class RecursiveClass
+    {
+        public static void RecursiveCall()
+        {
+            var dateTime = DateTime.Now;
+            RecursiveCall();
+        }
+    }
+}
---
+++ FunctionApp/TestFunction.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace DurableFunctionTestApp
+{
+    public static class TestFunction
+    {
+        [FunctionName("MethodAnalyzerTestCases")]
+        public static async Task Run([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            RecursiveClass.RecursiveCall();
+        }
+    }
+}
